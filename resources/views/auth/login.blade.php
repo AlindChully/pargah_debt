2b5db0d125c3a179d7d806d1e87d5ac7b03262bd
AlindChully <109308991+AlindChully@users.noreply.github.com>
2026-01-20 01:03:47 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,45 +27,6 @@
 </head>
 
 <body>
-    <div class="modal fade" id="addUserModal" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-
-                <div class="modal-header bg-primary text-white d-flex justify-content-between align-items-center">
-                    <h5 class="modal-title fw-bold mb-0">
-                        <i class="bi bi-person-plus"></i> {{ __('general.add user') }} <span id="addUserModal"></span>
-                    </h5>
-                    <button type="button" class="custom-close-btn" data-bs-dismiss="modal">✖</button>
-                </div>
-
-                <form action="{{ route('users.store') }}" method="POST">
-                    @csrf
-                    <div class="modal-body">
-                        <div class="row g-3">
-                            <div class="col-md-6">
-                                <label>{{ __('general.name') }}</label>
-                                <input type="text" name="name" class="form-control" required>
-                            </div>
-
-                            <div class="col-md-6">
-                                <label>{{ __('general.phone') }}</label>
-                                <input type="text" name="phone" class="form-control" required>
-                            </div>
-
-                            <div class="col-md-6">
-                                <label>{{ __('general.password') }}</label>
-                                <input type="password" name="password" class="form-control" required>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="modal-footer">
-                        <button class="btn btn-primary w-100">💾 {{ __('general.add user') }}</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
 
     <div class="container">
         <div class="row justify-content-center align-items-center" style="height:100vh;">
@@ -115,14 +76,6 @@
 
                         <div class="mt-3 text-center">
                             <p style="font-weight: bold;">@ By Alind Chuly Software</p>
-                            <button class="btn btn-primary btn3D mb-3 d-none d-md-inline-block" data-bs-toggle="modal"
-            data-bs-target="#addUserModal">
-            <i class="bi bi-person-plus"></i> {{ __('general.add user') }}
-        </button>
-                            <a href="{{ route('users.index') }}" target="_blank"
-            class="btn btn-primary btn3D mb-3 d-none d-md-inline-block">
-            <i class="bi bi-person"></i> {{ __('general.users') }}
-        </a>
                         </div>
                     </div>
                 </div>
