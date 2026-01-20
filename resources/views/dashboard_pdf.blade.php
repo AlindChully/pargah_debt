501c1e116e866c22d8e9a2d62b1fb8a7373136f4
AlindChully <109308991+AlindChully@users.noreply.github.com>
2026-01-21 02:21:38 +0300

    Update dashboard_pdf.blade.php

--- resources/views/dashboard_pdf.blade.php
+++ resources/views/dashboard_pdf.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard Report (PDF)</title>
-
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('images/logo.png') }}">
     <style>
     body {
         font-family: DejaVu Sans, Tahoma, Arial;
